5eaa4287e92931cda301dfaef9e0311bc4f772aa
Matthew Wood <mattwood2855@gmail.com>
2026-07-19 13:07:21 -0700

    auto scroll on chats

--- src/CSweet.UI/wwwroot/js/communications.js
+++ src/CSweet.UI/wwwroot/js/communications.js
@@ -1,3 +1,8 @@
 export function isDocumentVisible() {
     return document.visibilityState === "visible";
 }
+
+export function scrollToBottom(element) {
+    if (!element) return;
+    element.scrollTop = element.scrollHeight;
+}
